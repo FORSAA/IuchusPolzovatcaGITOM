dcd8ff60159530b133abd6ffe2f05b961e7cf188
El_makso <El_makso@mail.ru>
2022-11-29 18:35:06 +0300

    initial

---
+++ Product.py
@@ -0,0 +1,31 @@
+class Product:
+
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.__price = price
+        self.__salePercent = 0
+
+    @property
+    def salePrice(self):
+        return self.__price - self.__price*(self.__salePercent/100)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if(price>0):
+            self.__price=price
+
+    def Sale(self, percent):
+        if(percent <= 100 and percent >= 0):
+            self.__salePercent=percent
+
+    def GetFullPrice(self, count, is_on_sale):
+        if is_on_sale:
+            price = self.salePrice*count
+        else:
+            price = self.__price * count
+        return price
---
+++ main.py
@@ -0,0 +1,6 @@
+from Product import *
+
+a = Product(1, "Tomato", 100)
+a.Sale(10)
+print(a.salePrice)
+
